a384c2484c4574c60550259c7b852a3c15e0b02a
Clint Edwards <cedwards1080@gmail.com>
2021-08-04 21:07:09 -0600

    Redid the project so i can remember this stuff

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+let message = document.querySelector(`#message`)
+
 function addMovie(e) {
     e.preventDefault();
-    inputField = document.querySelector(`input`).value;
+    const inputField = document.querySelector(`input`);
     const movie = document.createElement(`li`);
     const movieTitle = document.createElement(`span`);
 
-    movieTitle.textContent = inputField;
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener(`click`, crossOffMovie)
+    const deleteBtn = document.createElement(`button`);
     movie.appendChild(movieTitle);
-    document.querySelector(`ul`).appendChild(movie) // Attaching the <ul></ul><span></span>
+    movie.appendChild(deleteBtn);
+    deleteBtn.textContent = `X`;
+    deleteBtn.addEventListener(`click`, deleteMovie);
     
+    document.querySelector(`ul`).appendChild(movie); // Attaching the <ul></ul><span></span>
+    inputField.value = ``;
+}
+function deleteMovie(e) {
+    e.target.parentNode.remove()
+    console.log(e.target.parentNode.firstChild.textContent)
+    message.textContent = `${e.target.parentNode.firstChild.textContent} deleted!`
+    revealMessage();
 }
 
+function crossOffMovie(e) {
+    e.target.classList.toggle(`checked`);
+    if(e.target.classList.contains(`checked`)) {
+        message.textContent = `${e.target.textContent} watched`;
+    } else {
+        message.textContent = `${e.target.textContent} added back`;
+    }
+    revealMessage();
+}
+function revealMessage(e) {
+    setTimeout(function () {
+        message.classList.toggle(`hide`);
+    }, 1000)
+}
 document.querySelector(`form`).addEventListener(`submit`, addMovie);
